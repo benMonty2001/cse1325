a54b3c9647a056dafee15c600e5371ed417f22b3
Benjamin Montgomery <73138581+benMonty2001@users.noreply.github.com>
2022-01-21 08:30:05 -0600

    Update Hello.java

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args){
 		System.out.print("Enter username: ");
-
-		//create instance of Scanner called userInterface
 											//specify origin
 		Scanner userInterface = new Scanner(System.in);
 		String name = userInterface.nextLine();
