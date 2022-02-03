076c67608114682129a7cace5aec2ac0eb50b046
Benjamin Montgomery <benMonty2001@gmail.com>
2022-02-02 23:38:42 -0600

    full credit

---
+++ P03/full_credit/Quint.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Quint {
+	public static void main(String[] args){
+		
+		Puzzle puzzle = new Puzzle(WordList.getWord());
+
+		System.out.println("=========\n"
+						 + "Q U I N T\n"
+						 + "=========\n");
+		System.out.print("Guess a 5-letter word" + 
+					     " guess ");
+
+		int counter = 0;
+		while(true){
+			Scanner in = new Scanner(System.in);
+			String guess = in.nextLine();
+			if(guess.length() != 5){
+				System.out.println("[GUESS NOT 5 LETTERS]");
+			} else {
+				System.out.print(puzzle.compareGuess(guess) + " ");
+				counter++;
+			}
+
+			if(puzzle.isSolved()){
+				System.out.printf("Guessed in %d tries\n", counter);
+				break;
+			}
+		}
+	}
+}
---
+++ P03/full_credit/Word.java
@@ -0,0 +1,22 @@
+class Word {
+	private char[] letters;
+
+	public Word(String word){
+		this.letters = word.toCharArray();
+	}
+
+	public char charAt(int position){
+		char letter = letters[position];
+		return letter;
+	}
+
+	public void setCharAt(char c, int position){
+		letters[position] = c;
+	}
+
+	@Override
+	public String toString(){
+		String str = new String(letters);
+		return str; 
+	}
+}
