df8d9ae5db9cb64e100a3b15ab8606b14e6ccd2c
Benjamin Montgomery <benMonty2001@gmail.com>
2022-02-04 06:55:18 -0600

    bonus

--- P03/bonus/Word.java
+++ P03/bonus/Word.java
@@ -4,9 +4,7 @@ class Word {
 	private boolean containsNonAlpha(String word){
 		char[] arr = word.toCharArray();
 		for(int i = 0; i < arr.length; i++){
-			if(!Character.isLetter(arr[i]) && 
-				                   arr[i] != ' ' &&
-                                   arr[i] != '.'){
+			if(!Character.isLetter(arr[i]) && arr[i] != ' ' && arr[i] != '.'){
 				return true;
 			}
 		}
@@ -32,9 +30,7 @@ class Word {
 		if(position > 4){
 			throw new IndexOutOfBoundsException("Index out of bounds!");
 		}
-		if(!Character.isLetter(c) && 
-                               c != ' ' &&
-                               c != '.'){
+		if(!Character.isLetter(c) && c != ' ' && c != '.'){
 			throw new IllegalArgumentException("Invalid guess: Not 5 letters!");		
 		}
 		letters[position] = c;
