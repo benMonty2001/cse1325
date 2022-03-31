7866e3806142951ee9c98c124bf27bf7574f21e9
Benjamin Montgomery <benMonty2001@gmail.com>
2022-03-31 12:05:22 -0500

    removed vector arguments

--- P08/bonus/reverse.cpp
+++ P08/bonus/reverse.cpp
@@ -3,7 +3,6 @@
 #include <algorithm>
 
 int main(int argc, char* argv[]) {	
-	std::vector<std::string> arguments(argv, argv + argc);
 	for(int i = 0; i < argc; i++) {
 		std::string s{argv[i]};
 		std::reverse(s.begin(), s.end());
